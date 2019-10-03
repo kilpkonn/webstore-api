3691d83de7d2026d53c59f0b262b8e2ccbda57ee
tajoot <tajoot@ttu.ee>
2019-10-03 17:15:10 +0000

    Revert "Create products frontend"
    This reverts commit 352ba59bfd8713f9e8851b5aa0c142347f716735

--- backend/src/main/java/ee/taltech/iti0203/webstore/controller/ProductController.java
+++ backend/src/main/java/ee/taltech/iti0203/webstore/controller/ProductController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/products")
+@RequestMapping("/products")
 public class ProductController {
 
     private final ProductRepository repository;
