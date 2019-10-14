f13f1416bef946926539cb297118066f2b7463bd
tajoot <tajoot@ttu.ee>
2019-10-14 18:55:55 +0300

    Remove unnecessary imports

--- src/main/java/ee/taltech/iti0203/webstore/WebStoreApplication.java
+++ src/main/java/ee/taltech/iti0203/webstore/WebStoreApplication.java
@@ -1,15 +1,7 @@
 package ee.taltech.iti0203.webstore;
 
-import ee.taltech.iti0203.webstore.model.News;
-import ee.taltech.iti0203.webstore.model.Product;
-import ee.taltech.iti0203.webstore.repository.NewsRepository;
-import ee.taltech.iti0203.webstore.repository.ProductRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import java.util.List;
 
 @SpringBootApplication
 public class WebStoreApplication {
