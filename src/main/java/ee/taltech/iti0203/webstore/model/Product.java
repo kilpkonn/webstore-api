e92684737105f8622cafd0ae6f7f4e4dd620218a
tajoot <tajoot@ttu.ee>
2019-10-14 19:48:41 +0300

    Add first integration test

--- src/main/java/ee/taltech/iti0203/webstore/model/Product.java
+++ src/main/java/ee/taltech/iti0203/webstore/model/Product.java
@@ -12,7 +12,7 @@ import javax.persistence.*;
 public class Product {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String description;
