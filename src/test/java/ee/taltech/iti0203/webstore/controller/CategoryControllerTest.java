38da590157d34dd6f0ff43d20dfe0e22024587a7
taannu <taannu@ttu.ee>
2019-11-08 22:52:28 +0200

    Make use of @Before and @After in tests

--- src/test/java/ee/taltech/iti0203/webstore/controller/CategoryControllerTest.java
+++ src/test/java/ee/taltech/iti0203/webstore/controller/CategoryControllerTest.java
@@ -1,6 +1,10 @@
 package ee.taltech.iti0203.webstore.controller;
 
+import ee.taltech.iti0203.webstore.model.Category;
 import ee.taltech.iti0203.webstore.pojo.CategoryDto;
+import ee.taltech.iti0203.webstore.repository.CategoryRepository;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,7 +20,10 @@ import org.springframework.test.context.junit4.SpringRunner;
 import java.util.List;
 
 import static org.apache.commons.collections4.CollectionUtils.isNotEmpty;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 import static org.springframework.http.HttpMethod.POST;
 
 @RunWith(SpringRunner.class)
@@ -26,8 +33,26 @@ public class CategoryControllerTest {
     @Autowired
     private TestRestTemplate template;
 
+    @Autowired
+    private CategoryRepository repository;
+
     private static final ParameterizedTypeReference<List<CategoryDto>> LIST_OF_CATEGORIES = new ParameterizedTypeReference<>() {
     };
+
+    @Before
+    public void setUp() {
+        repository.deleteAll();  // Just in case
+
+        repository.save(new Category("Category 1"));
+        repository.save(new Category("Category 2"));
+        repository.save(new Category("Category 3"));
+        repository.save(new Category("Category 4"));
+    }
+
+    @After
+    public void cleanUp() {
+        repository.deleteAll();
+    }
 
     @Test
     public void application_returns_list_of_categories() {
@@ -35,44 +60,74 @@ public class CategoryControllerTest {
         assertEquals(HttpStatus.OK, entity.getStatusCode());
         List<CategoryDto> category = entity.getBody();
         assertTrue(isNotEmpty(category));
-        assertTrue(category.stream().anyMatch(p -> p.getName().equals("red")));
+        assertTrue(category.stream().anyMatch(p -> p.getName().equals("Category 1")));
+        assertTrue(category.stream().anyMatch(p -> p.getName().equals("Category 3")));
     }
 
     @Test
     public void can_get_category_by_id() {
-        ResponseEntity<CategoryDto> entity = template.exchange("/categories/100", HttpMethod.GET, null, CategoryDto.class);
+        CategoryDto category = new CategoryDto();
+        category.setName("Category 6");
+        HttpEntity<CategoryDto> httpEntity = new HttpEntity<>(category);
+        ResponseEntity<CategoryDto> entity = template.exchange("/categories", POST, httpEntity, CategoryDto.class);
+        assertNotNull(entity.getBody());
+        entity = template.exchange("/categories/" + entity.getBody().getId(), HttpMethod.GET, null,
+                CategoryDto.class);
         assertNotNull(entity);
-        CategoryDto category = entity.getBody();
+        category = entity.getBody();
         assertNotNull(category);
-        assertEquals("green", category.getName());
+        assertEquals("Category 6", category.getName());
     }
 
     @Test
     public void can_add_categories() {
         CategoryDto category = new CategoryDto();
-        category.setId(900L);
-        category.setName("crimson");
+        category.setName("Category 5");
         HttpEntity<CategoryDto> httpEntity = new HttpEntity<>(category);
         ResponseEntity<CategoryDto> entity = template.exchange("/categories", POST, httpEntity, CategoryDto.class);
         assertEquals(HttpStatus.OK, entity.getStatusCode());
         CategoryDto categoryItem = entity.getBody();
         assertNotNull(categoryItem);
-        assertEquals("crimson", categoryItem.getName());
+        assertEquals("Category 5", categoryItem.getName());
     }
 
     @Test
     public void can_delete_category_by_id() {
         CategoryDto dummyCategory = new CategoryDto();
-        dummyCategory.setName("poisonous");
-        dummyCategory.setId(999L);
+        dummyCategory.setName("Category 3");
+        dummyCategory.setId(3L);
+        ResponseEntity<CategoryDto> entity = template.exchange("/categories", POST, new HttpEntity<>(dummyCategory), CategoryDto.class);
+        assertNotNull(entity);
+        assertNotNull(entity.getBody());
+        assertNotNull(entity.getBody().getName());
+
+        template.exchange("/categories/3", HttpMethod.DELETE, null, CategoryDto.class);
+        entity = template.exchange("/categories/3", HttpMethod.GET, null, CategoryDto.class);
+        assertNotNull(entity);
+        assertNotNull(entity.getBody());
+        assertNull(entity.getBody().getName());
+    }
+
+    @Test
+    public void can_rename_category() {
+        CategoryDto dummyCategory = new CategoryDto();
+        dummyCategory.setName("oldName");
         ResponseEntity<CategoryDto> entity = template.exchange("/categories", POST, new HttpEntity<>(dummyCategory), CategoryDto.class);
         assertNotNull(entity);
         assertNotNull(entity.getBody());
 
-        template.exchange("/categories/999", HttpMethod.DELETE, null, CategoryDto.class);
-        entity = template.exchange("/categories/999", HttpMethod.GET, null, CategoryDto.class);
+        entity = template.exchange("/categories/" + entity.getBody().getId(), HttpMethod.GET, null, CategoryDto.class);
+
+        assertNotNull(entity.getBody());
+        CategoryDto renamedDummyCategory = new CategoryDto();
+        renamedDummyCategory.setName("newName");
+        renamedDummyCategory.setId(entity.getBody().getId());
+
+        template.exchange("/categories/" + entity.getBody().getId(), HttpMethod.PUT, new HttpEntity<>(renamedDummyCategory),
+                CategoryDto.class);
+        entity = template.exchange("/categories/" + entity.getBody().getId(), HttpMethod.GET, null, CategoryDto.class);
         assertNotNull(entity);
         assertNotNull(entity.getBody());
-        assertNull(entity.getBody().getName());
+        assertEquals("newName", entity.getBody().getName());
     }
 }
