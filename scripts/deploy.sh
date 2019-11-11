76cf021c15adfa1385b358a7ac4502efe2c05731
taannu <taannu@ttu.ee>
2019-11-11 21:34:50 +0200

    Test colors and migration

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-echo "Pulling image"
+echo "\033[0;36mPulling image\033[0m"
 docker pull "$CI_REGISTRY_USER"/"$CI_REGISTRY_REPOSITORY":"$CI_COMMIT_SHORT_SHA"
 
 docker container ls -a -s
-echo "Stopping container $APP_CONTAINER_NAME"
+echo "\033[0;36mStopping container \033[43m$APP_CONTAINER_NAME\033[0m"
 docker stop "$APP_CONTAINER_NAME" || true
-echo "Removing $APP_CONTAINER_NAME"
+echo "\033[0;36mRemoving $APP_CONTAINER_NAME\033[0m"
 docker rm "$APP_CONTAINER_NAME" || true
 docker container ls -a -s
 
-echo "Starting new container: $APP_CONTAINER_NAME"
+echo "\033[0;36mStarting new container: \033[43m$APP_CONTAINER_NAME\033[0m"
 docker run -e "SPRING_PROFILES_ACTIVE=prod" \
    --name "$APP_CONTAINER_NAME" \
    -p 8080:8080 \
