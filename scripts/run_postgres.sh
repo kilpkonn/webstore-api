1c28e876782865d433bdffa2552a950da5022d20
taannu <taannu@ttu.ee>
2019-11-11 21:06:07 +0200

    Use random db pass every time

--- scripts/run_postgres.sh
+++ scripts/run_postgres.sh
@@ -3,12 +3,19 @@
 export DB_PASS="fudd386a61h2sdsbn3bu3bi37873bdabd3b73ada56yxvnm4y737ihsgf"
 #export DB_PASS=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
 
+echo "Pulling postgres:latest"
 docker pull postgres
+echo "Stopping container: $DATABASE_CONTAINER_NAME"
 docker stop "$DATABASE_CONTAINER_NAME" || true
+echo "Removing container: $DATABASE_CONTAINER_NAME"
 docker rm "$DATABASE_CONTAINER_NAME" || true
 
+docker container ls -a -s
+
+echo "Creating network bridge (if none exsists)"
 docker network create --driver bridge postgres-network || true # Create only if none exists
 
+echo "Starting $DATABASE_CONTAINER_NAME"
 docker run \
    -e "POSTGRES_USER=postgres" \
    -e "POSTGRES_PASSWORD=$DB_PASS" \
