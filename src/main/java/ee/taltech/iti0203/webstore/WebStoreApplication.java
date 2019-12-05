461b405983318b7871f883104f0cb6338c9044b8
Taavi Jõõts <tajoot@ttu.ee>
2019-12-05 08:40:23 +0000

    Revert "Move admin from bean to securityconfig"
    This reverts commit 1f5fe26ac4a3fa04991de1f6aa03ed1e971bc7ad

--- src/main/java/ee/taltech/iti0203/webstore/WebStoreApplication.java
+++ src/main/java/ee/taltech/iti0203/webstore/WebStoreApplication.java
@@ -1,11 +1,34 @@
 package ee.taltech.iti0203.webstore;
 
+import ee.taltech.iti0203.webstore.model.User;
+import ee.taltech.iti0203.webstore.repository.UserRepository;
+import ee.taltech.iti0203.webstore.security.Role;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.annotation.Resource;
 
 @SpringBootApplication
 public class WebStoreApplication {
+
+	@Resource
+	private PasswordEncoder passwordEncoder;
+
 	public static void main(String[] args) {
 		SpringApplication.run(WebStoreApplication.class, args);
 	}
+
+	@Bean
+	public CommandLineRunner initStore(UserRepository repo) {
+		return (args) -> {
+			User admin = new User();
+	    	admin.setUsername("admin");
+	    	admin.setPassword(passwordEncoder.encode("nimda"));
+	    	admin.setRole(Role.ADMIN);
+	    	repo.save(admin);
+		};
+  	}
 }
