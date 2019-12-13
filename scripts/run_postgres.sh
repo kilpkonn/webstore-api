61d85bac0e1409d7ceaea7198621022e604f9796
taannu <taannu@ttu.ee>
2019-12-13 09:43:25 +0200

    Add colors to ron_postgres.sh

--- scripts/run_postgres.sh
+++ scripts/run_postgres.sh
@@ -1,33 +1,70 @@
 #!/bin/bash
 
-# export DB_PASS="fudd386a61h2sdsbn3bu3bi37873bdabd3b73ada56yxvnm4y737ihsgf"
-export DATABASE_CONTAINER_NAME="postgres-container"
+export TERM=xterm-256color
+echo $TERM
 
+# Reset
+export Color_Off='\033[0m'       # Text Reset
+
+# Regular Colors
+export Black='\033[0;30m'        # Black
+export Red='\033[0;31m'          # Red
+export Green='\033[0;32m'        # Green
+export Yellow='\033[0;33m'       # Yellow
+export Blue='\033[0;34m'         # Blue
+export Purple='\033[0;35m'       # Purple
+export Cyan='\033[0;36m'         # Cyan
+
+# High Intensity
+export IBlack='\033[0;90m'       # Black
+export IRed='\033[0;91m'         # Red
+export IGreen='\033[0;92m'       # Green
+export IYellow='\033[0;93m'      # Yellow
+export IBlue='\033[0;94m'        # Blue
+export IPurple='\033[0;95m'      # Purple
+export ICyan='\033[0;96m'        # Cyan
+export IWhite='\033[0;97m'       # White
+
+# Bold High Intensity
+export BIBlack='\033[1;90m'      # Black
+export BIRed='\033[1;91m'        # Red
+export BIGreen='\033[1;92m'      # Green
+export BIYellow='\033[1;93m'     # Yellow
+export BIBlue='\033[1;94m'       # Blue
+export BIPurple='\033[1;95m'     # Purple
+export BICyan='\033[1;96m'       # Cyan
+export BIWhite='\033[1;97m'      # White
+
+export DATABASE_CONTAINER_NAME="postgres-container"
+echo -e "${IYellow}"
 docker container ls -a -s
 
 if [ ! "$(docker ps -q -f name="$DATABASE_CONTAINER_NAME")" ]; then
     if [ "$(docker ps -aq -f status=exited -f name="$DATABASE_CONTAINER_NAME")" ]; then
         # cleanup
+        echo -e "${Purple}"
         docker rm "$DATABASE_CONTAINER_NAME"
     fi
 
-    echo "Pulling postgres"
+    echo -e "${BICyan}Pulling postgres${Blue}"
     docker pull postgres
 
+    echo -e "${Yellow}"
     docker container ls -a -s
-    echo "Stopping container: $DATABASE_CONTAINER_NAME"
+    echo -e "${BICyan}Stopping container:${BIYellow} $DATABASE_CONTAINER_NAME ${Purple}"
     docker stop "$DATABASE_CONTAINER_NAME" || true
-    echo "Removing container: $DATABASE_CONTAINER_NAME"
+    echo -e "${BICyan}Removing container:${BIYellow} $DATABASE_CONTAINER_NAME ${Purple}"
     docker rm "$DATABASE_CONTAINER_NAME" || true
 
+    echo -e "${Green}"
     docker container ls -a -s
 
-    echo "Creating internal network bridge for proxy-back-database (if none exsists)"
+    echo -e "${BICyan}Creating internal network bridge for proxy-back-database (if none exsists) ${IRed}"
     docker network create --driver bridge api-internal-network || true # Create only if none exists
 
     export DB_PASS=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
 
-    echo "Starting $DATABASE_CONTAINER_NAME"
+    echo -e "${BICyan}Starting${BIYellow} $DATABASE_CONTAINER_NAME ${IPurple}"
     docker run \
         -e "POSTGRES_USER=postgres" \
         -e "POSTGRES_PASSWORD=$DB_PASS" \
@@ -39,10 +76,12 @@ if [ ! "$(docker ps -q -f name="$DATABASE_CONTAINER_NAME")" ]; then
         -d "postgres" \
         # -u "postgres" # Will own data folders
 
+    echo -e "${Green}"
     docker container ls -a -s
-    echo "Generated new password for database, make sure to write it down!"
+    echo -e "${BIGreen}Generated new password for database, make sure to write it down!${BPurple}"
     echo "$DB_PASS"
+    echo -e "${Color_Off}"
 else
-    echo "$DATABASE_CONTAINER_NAME seems to be running."
+    echo -e "${BIYellow}$DATABASE_CONTAINER_NAME ${BICyan}seems to be running.${Color_Off}"
 fi
 
